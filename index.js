b2e0080a054c5720e26812e4ba4e4ffb32a1b330
Alvin Nario <59873164+narioalvin@users.noreply.github.com>
2020-07-23 16:56:39 +0800

    remove public files

--- index.js
+++ index.js
@@ -29,12 +29,6 @@ app.use(cors());
 app.use('/api/user', authRoute);
 app.use('/api/transaction', transactionRoute);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(__dirname + '/public/'));
-
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
-}
-
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Running in port ${port}`));
